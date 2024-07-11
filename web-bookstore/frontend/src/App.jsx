76df4001f1397406eb7ce78a68399cdeb60da50a
rishi998 <mehtorishi62203@gmail.com>
2024-07-11 08:35:27 +0530

    new Project coming

---
+++ web-bookstore/frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./components/navbar";
+function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
